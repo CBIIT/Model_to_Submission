fdd625fb33e3b2297a2c89092e9674a330acd41d
svburke <42675174+svburke@users.noreply.github.com>
2022-10-26 10:47:00 -0500

    Update Model_to_Submission.R

--- Model_to_Submission.R
+++ Model_to_Submission.R
@@ -126,6 +126,7 @@ output_file=paste(file_name,"_Submission_Template.xlsx",sep="")
 
 #Create a preferred order of nodes. While hard coded, any additions to the data model will result with those nodes being placed at the end of the list.
 preferred_order=c("study","study_admin","study_arm","study_funding","study_personnel","publication","participant","diagnosis","therapeutic_procedure","sample","sequencing_file","methylation_array_file","imaging_file","clinical_measure_file","synonym")
+preferred_order=preferred_order[preferred_order %in% names(model$Nodes)]
 preferred_order=c(preferred_order,names(model$Nodes)[!names(model$Nodes) %in% preferred_order])
 
 #Create new Dictionary page
