1b1c8bd0370af470211d75101c289a1a89c38e82
svburke <sean.burke2@nih.gov>
2023-01-19 15:42:07 -0600

    Update Model_to_Submission.R

--- Model_to_Submission.R
+++ Model_to_Submission.R
@@ -136,11 +136,11 @@ preferred_order=preferred_order[preferred_order %in% names(model$Nodes)]
 preferred_order=c(preferred_order,names(model$Nodes)[!names(model$Nodes) %in% preferred_order])
 
 #Create new Dictionary page
-dd=data.frame(matrix(ncol = 11,nrow=0))
-dd_add=data.frame(matrix(ncol = 11,nrow=1))
+dd=data.frame(matrix(ncol = 9,nrow=0))
+dd_add=data.frame(matrix(ncol = 9,nrow=1))
 
-colnames(dd)<-c("Property","Description","Node","Type","Example value","Required","Key","CDE","CDE version","NCIt","Other Source")
-colnames(dd_add)<-c("Property","Description","Node","Type","Example value","Required","Key","CDE","CDE version","NCIt","Other Source")
+colnames(dd)<-c("Property","Description","Node","Type","Example value","Required","Key","CDE","CDE version")
+colnames(dd_add)<-c("Property","Description","Node","Type","Example value","Required","Key","CDE","CDE version")
 
 #Populate Dictionary page
 for (prop in names(model_props$PropDefinitions)){
@@ -208,16 +208,6 @@ for (x in 1:length(names(model_props$PropDefinitions))){
         df_prop_code=rbind(df_prop_code, df_prop_code_add)
       }
     }
-    if(any(grepl(pattern = "NCIt", x = model_props$PropDefinitions[[x]]["Term"][[1]]))){
-      num_codes=grep(pattern = "NCIt", x = model_props$PropDefinitions[[x]]["Term"][[1]])
-      for (y in num_codes){
-        df_prop_code_add$Property=names(model_props$PropDefinitions[x])
-        df_prop_code_add$Code=model_props$PropDefinitions[[x]]["Term"][[1]][[y]]["Code"][[1]]
-        df_prop_code_add$Version=NA
-        df_prop_code_add$Source="NCIt"
-        df_prop_code=rbind(df_prop_code, df_prop_code_add)
-      }
-    }
   }
 }
 
@@ -238,13 +228,6 @@ for (prop in 1:dim(dd)[1]){
             dd$`CDE version`[prop]= ver
           }
         }
-      }
-    }
-    if (any(grepl(pattern = "NCIt", x = prop_df$Source))){
-      prop_df_NCIt=filter(prop_df,Source=="NCIt")
-      codes=prop_df_NCIt$Code
-      if (!is.null(codes)){
-        dd$NCIt[prop]=codes
       }
     }
   }
