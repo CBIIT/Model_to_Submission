fbd1ca9a2a22b83fff0b4971b11844c541a26d20
svburke <sean.burke2@nih.gov>
2023-10-13 10:20:12 -0500

    Update Model_to_Submission.R

--- Model_to_Submission.R
+++ Model_to_Submission.R
@@ -131,7 +131,7 @@ output_file=paste(file_name,
 #################
 
 #Create a preferred order of nodes. While hard coded, any additions to the data model will result with those nodes being placed at the end of the list.
-preferred_order=c("study","study_admin","study_arm","study_funding","study_personnel","publication","participant","family_relationship","diagnosis","therapeutic_procedure","medical_history","exposure",'radiology_file',"follow_up","molecular_test","sample","cell_line","pdx","sequencing_file","clinical_measure_file","methylation_array_file","pathology_file","single_cell_sequencing_file","synonym")
+preferred_order=c("study","study_admin","study_arm","study_funding","study_personnel","publication","participant","family_relationship","diagnosis","therapeutic_procedure","medical_history","exposure",'radiology_file',"follow_up","molecular_test","sample","cell_line","pdx","sequencing_file","clinical_measure_file","methylation_array_file","cytogenomic_file","pathology_file","single_cell_sequencing_file","synonym")
 preferred_order=preferred_order[preferred_order %in% names(model$Nodes)]
 preferred_order=c(preferred_order,names(model$Nodes)[!names(model$Nodes) %in% preferred_order])
 
